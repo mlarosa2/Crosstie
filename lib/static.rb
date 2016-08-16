4abdfbf42f52278a7f80f0f7e25940037cf5c5c9
Mark LaRosa <mark.larosa2@gmail.com>
2016-08-16 16:59:22 -0400

    add exceptions and static assets serving to middleware

--- lib/static.rb
+++ lib/static.rb
@@ -1,7 +1,27 @@
 class Static
+  attr_reader :app
+  MIME = {
+    ".jpg" => "image/jpeg",
+    ".jpeg" => "image/jpeg",
+    ".png" => "image/png",
+    ".gif" => "image/gif",
+    ".css" => "text/css",
+    ".js" => "application/javascript",
+    ".json" => "application/json",
+    ".txt" => "text/plain",
+    ".zip" => "application/zip"
+  }
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    req = Rack::Request.new(env)
+    begin
+      file = File.new(__dir__ + req.path)
+      res = ["200", { "Content-type" => MIME[File.extname(file)] }, [file.read]]
+    rescue
+      res = ["404", { "Content-type" => "text/html" }, ['Could not find file, sorry.']]
+    end
   end
 end
