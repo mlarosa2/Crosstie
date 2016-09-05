edc73a0dd50c6cbef1541acba5aad07c1d5ac0f7
Mark LaRosa <mark.larosa2@gmail.com>
2016-09-05 17:12:22 -0400

    fix issue where static assets were always trying to be served

--- lib/initializers/static.rb
+++ lib/initializers/static.rb
@@ -1,29 +1,32 @@
-Module CrosstieInit
-  class Static
-    attr_reader :app
-    MIME = {
-      ".jpg" => "image/jpeg",
-      ".jpeg" => "image/jpeg",
-      ".png" => "image/png",
-      ".gif" => "image/gif",
-      ".css" => "text/css",
-      ".js" => "application/javascript",
-      ".json" => "application/json",
-      ".txt" => "text/plain",
-      ".zip" => "application/zip"
-    }
-    def initialize(app)
-      @app = app
-    end
+class Static
+  attr_reader :app
+  MIME = {
+    ".jpg" => "image/jpeg",
+    ".jpeg" => "image/jpeg",
+    ".png" => "image/png",
+    ".gif" => "image/gif",
+    ".css" => "text/css",
+    ".js" => "application/javascript",
+    ".json" => "application/json",
+    ".txt" => "text/plain",
+    ".zip" => "application/zip"
+  }
+  def initialize(app)
+    @app = app
+    @res = Rack::Response.new
+  end
 
-    def call(env)
-      req = Rack::Request.new(env)
+  def call(env)
+    req = Rack::Request.new(env)
+    if req.path =~ (/^\/lib\/public/)
       begin
-        file = File.new(__dir__ + req.path)
+        file = File.new(__dir__ + '/../..' + req.path)
         res = ["200", { "Content-type" => MIME[File.extname(file)] }, [file.read]]
       rescue
         res = ["404", { "Content-type" => "text/html" }, ['Could not find file, sorry.']]
       end
+    else
+      app.call(env)
     end
   end
 end
